37ff1b2f806e9280d0966a58e1a1719e8e86ec5a
Di Xu <xudifsd@gmail.com>
2019-03-29 22:33:33 +0800

    add config parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,74 @@
 use std::process::{Command, Stdio};
 
+use toml::Value;
+use dirs;
+
+use std::path::Path;
+use std::fs::File;
+
+use std::io;
 use std::io::{BufRead, BufReader};
+use std::io::prelude::*;
 
 use clap::{Arg, App};
 
-fn main() {
+enum LogMode {
+    TEE,
+    REDIRECT,
+}
+
+struct FileConfig {
+    mode: LogMode,
+    num: i32, // default to 5
+    time: u32, // timestamp integer, 0 means no limit
+    size: u32, // byte, 0 means no limit
+}
+
+impl FileConfig {
+    fn new() -> Self {
+        FileConfig {mode: LogMode::REDIRECT, num: 5, time: 0, size: 0}
+    }
+}
+
+struct LogConfig {
+    mlog: FileConfig,
+    stdout: FileConfig,
+    stderr: FileConfig,
+}
+
+impl LogConfig {
+    fn new() -> Self {
+        LogConfig {mlog: FileConfig::new(), stdout: FileConfig::new(), stderr: FileConfig::new()}
+    }
+
+    fn parse(&mut self, config: Value) {
+        println!("{:?}", config["stdout"]); // TODO
+    }
+}
+
+fn get_config(config_path: Option<&str>) -> Result<LogConfig, std::io::Error> {
+    let file_path = if config_path.is_some() {
+        Path::new(config_path.unwrap()).to_path_buf()
+    } else {
+        let home = dirs::home_dir().expect("Could not find home dir");
+        Path::new(&home).join(".mlog")
+    };
+
+    let mut f = File::open(file_path.to_str().unwrap()).expect("failed to open");
+
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).expect("failed to read");
+    let content = std::str::from_utf8(&buffer).unwrap();
+
+    let value = content.parse::<Value>().unwrap();
+
+    let mut result = LogConfig::new();
+    result.parse(value);
+
+    Ok(result)
+}
+
+fn main() -> io::Result<()> {
     let matches = App::new("Manage cmd logs for you")
                           .version("0.1")
                           .author("Di Xu <xudifsd@gmail.com>")
@@ -18,7 +82,7 @@ fn main() {
                                .last(true))
                           .get_matches();
 
-    let _config = matches.value_of("config"); // TODO read config
+    let config = get_config(matches.value_of("config"));
 
     let cmd = matches.values_of("cmd").map(|vals| vals.collect::<Vec<_>>()).unwrap();
 
@@ -27,8 +91,7 @@ fn main() {
         // do not redirect stdin, let cmd inherented from mlog
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
-        .spawn()
-        .expect("failed to start");
+        .spawn()?;
 
     let stdout = child.stdout.expect("Could not capture standard output.");
     let stderr = child.stderr.expect("Could not capture standard error.");
@@ -43,4 +106,6 @@ fn main() {
     err_reader.lines()
         .filter_map(|line| line.ok())
         .for_each(|line| eprintln!("err: {}", line));
+
+    Ok(())
 }
--- t.sh
+++
@@ -1,2 +0,0 @@
-echo error >&2
-echo out
